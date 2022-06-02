148e134f932debc4096e354ade24a0861e151473
chihaoyo <chihaoyo@gmail.com>
2022-06-02 13:39:51 +0800

    First commit

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="page index"></div>
+</template>
+
+<script>
+export default {
+  asyncData({ app, query }) {
+    const url = 'https://docs.google.com/document/d/e/2PACX-1vTDqM9y8HpAPL9J7RaYfcXF9eBJoVngDK5FGhP0GnkKCygEd63QHsNpslp1pA9ucon4xnaxwwIynQ95/pub'
+    return {
+      url
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
